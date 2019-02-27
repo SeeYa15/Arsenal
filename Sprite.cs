09fb7fa93488ce6d666917b0eb9d4871728d39b7
Johnny Svensson <Johnny.Svensson15@gmail.com>
2019-02-27 14:26:04 +0100

    Added functionallity with animation. Need more fix doe

---
+++ Sprite.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arsenal
+{
+    class Sprite
+    {
+        public Vector2 Position { get; set; }
+        public Texture2D Texture { get; set; }
+        public Rectangle Bounds
+        {
+            get
+            {
+                return new Rectangle((int)Position.X,(int)Position.Y,Texture.Width, Texture.Height);
+            }
+        }
+        public SpriteBatch SpriteBatch;
+
+        public Sprite(Vector2 position, Texture2D texture, SpriteBatch spriteBatch)
+        {
+            Position = position;
+            Texture = texture;
+            SpriteBatch = spriteBatch;
+        }
+
+        public virtual void Draw()
+        {
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(Texture,Position, Color.White);            
+            SpriteBatch.End();
+        }
+
+        //public virtual void DrawWithRectangle(int row, int colum)
+        //{
+        //    SpriteBatch.Begin();
+        //    SpriteBatch.Draw(Texture, Position, new Rectangle(new Point(), new Point(Texture.Width/colum,Texture.Height/row)), Color.White);
+        //    SpriteBatch.End();
+        //}
+    }
+}   
